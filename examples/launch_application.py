0142a9bf04d9dc76fcd2df2a3e0f92c4bf8fdfa1
Greg Stoll <greg.stoll@ni.com>
2020-08-21 16:20:01 -0500

    clearer message on example

--- examples/launch_application.py
+++ examples/launch_application.py
@@ -2,6 +2,6 @@ from flexlogger import Application
 
 with Application() as a:
     project = a.open_project(path=r'C:\Users\gstoll\Documents\FlexLogger Projects\downsampling demo\downsampling demo.flxproj')
-    print("Press any key to close project...")
+    print("Press Enter to close project...")
     input()
     project.close(allow_prompts=False)
