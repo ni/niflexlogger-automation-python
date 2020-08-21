39c1b98685b6a719847d0bd69bc08dc9916a8f41
Greg Stoll <greg.stoll@ni.com>
2020-08-21 16:26:01 -0500

    make project path a command-line argument

--- examples/launch_application.py
+++ examples/launch_application.py
@@ -1,7 +1,14 @@
 from flexlogger import Application
+import os
+import sys
 
+if len(sys.argv) < 2:
+    print("Usage: %s <path of project to open>" % os.path.basename(__file__))
+    sys.exit(1)
+
+project_path = sys.argv[1]
 with Application() as a:
-    project = a.open_project(path=r'C:\Users\gstoll\Documents\FlexLogger Projects\downsampling demo\downsampling demo.flxproj')
+    project = a.open_project(path=project_path)
     print("Press Enter to close project...")
     input()
     project.close(allow_prompts=False)
