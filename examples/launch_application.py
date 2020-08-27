77233ee208336e86f84b7917169a6acfcd999398
Greg Stoll <greg.stoll@ni.com>
2020-08-27 15:23:37 -0500

    Use main() method

--- examples/launch_application.py
+++ examples/launch_application.py
@@ -3,13 +3,22 @@ import sys
 
 from flexlogger import Application
 
-if len(sys.argv) < 2:
-    print("Usage: %s <path of project to open>" % os.path.basename(__file__))
-    sys.exit(1)
 
-project_path = sys.argv[1]
-with Application() as a:
-    project = a.open_project(path=project_path)
-    print("Press Enter to close project...")
-    input()
-    project.close(allow_prompts=False)
+def main(argv=None):
+    """Launch FlexLogger and open a project"""
+    if argv is None:
+        argv = sys.argv
+    if len(argv) < 2:
+        print("Usage: %s <path of project to open>" % os.path.basename(__file__))
+        return 1
+
+    project_path = argv[1]
+    with Application() as a:
+        project = a.open_project(path=project_path)
+        print("Press Enter to close project...")
+        input()
+        project.close(allow_prompts=False)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
